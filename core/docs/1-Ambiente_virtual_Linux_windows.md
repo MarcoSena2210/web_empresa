a1d6f714f71e50c63fc6ce1bb6a9019213bc69b3
MarcoSena2210MarcoSena2210 <msena2210@gmail.com>
2023-11-01 13:39:35 -0300

    1-Instalando o dotenv,2-Criando variaveis de ambiente usando e .env,3-Configurando o settings.py

---
+++ core/docs/1-Ambiente_virtual_Linux_windows.md
@@ -0,0 +1,77 @@
+1-Ambiente Virtual Linux/Windows
+Dev: Marco Sena
+Creditos: :Professora  Letícia Lima 
+Criar o ambiente virtual Linux/Windows
+
+## Windows
+
+python -m venv .venv
+source .venv/Scripts/activate # Ativar ambiente
+
+## Linux  
+
+## Caso não tenha virtualenv. "pip install virtualenv"
+
+virtualenv .venv
+source .venv/bin/activate # Ativar ambiente
+
+## Instalar os seguintes pacotes Iniciais
+
+pip install django
+pip install pillow # tratamento de imagem
+
+## Para criar o arquivo requirements.txt
+
+### Cria o arquivo contendo todas asdependências do nosso projeto
+
+pip freeze > requirements.txt
+
+## Para criar as dependencias lendo o arquivo requirements.txt
+
+### Usariamos
+
+### pip install -r requirements.txt
+
+### seria instalado as dependecias baseada nesse arquivo  
+
+### Criando o projeto chamado Core
+
+“core” é nome do seu projeto e quando colocamos um “.” depois do nome do projeto significa que é para criar os arquivos na raiz da pasta. Assim não cria subpasta do projeto
+
+```
+django-admin startproject core .
+```
+
+### Testar a aplicação
+
+```
+python manage.py runserver
+```
+
+### Criando a 1ª Branch do projeto
+
+```
+git checkout -b parte01_config_static
+
+```
+
+### Comitando
+
+```
+git add .
+git commit -m "Configuração dos arquivos staticos" 
+
+```
+
+### Subindo o projeto para git
+
+```
+git push origin parte01_config_static
+
+```
+
+### Criando uma pasta collectstatic
+
+ ```
+python manage.py collectstatic  
+ ```
--- core/settings.py
+++ core/settings.py
@@ -9,7 +9,10 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
+# importar a biblioteca
 import os
+import sys
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 # base_dir config
@@ -17,22 +20,33 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 TEMPLATE_DIR = os.path.join(BASE_DIR,'templates')
 STATIC_DIR=os.path.join(BASE_DIR,'static')
 
+# Adicionar essa tag para que nosso projeto encontre o .env
+load_dotenv(os.path.join(BASE_DIR, ".env"))
+
+# Diz para Django onde estão nossos aplicativos
+APPS_DIR = str(os.path.join(BASE_DIR,'apps'))
+sys.path.insert(0, APPS_DIR)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
-
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-_k#3d+sgt$lw07@dlnf9fvjk2ir(id(8rp5%pi*d#8ows-#z$='
+# SECRET_KEY = 'django-insecure-_k#3d+sgt$lw07@dlnf9fvjk2ir(id(8rp5%pi*d#8ows-#z$='
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# Chamar as variaveis que estão no arquivo de variaveis assim
+SECRET_KEY = os.getenv("SECRET_KEY")
+# DEBUG
+DEBUG = os.getenv('DEBUG')
+# Se tiver configuração de email
+EMAIL_HOST = os.getenv('EMAIL_HOST')
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
+EMAIL_PORT = os.getenv('EMAIL_PORT')
+EMAIL_USE_TLS = os.getenv('EMAIL_USE_TLS')
+DEFAULT_FROM_EMAIL = os.getenv('DEFAULT_FROM_EMAIL')
+SERVER_EMAIL = DEFAULT_FROM_EMAIL
 
-ALLOWED_HOSTS = []
-
-
-# Application definition
-
-INSTALLED_APPS = [
+# Aplicativos do django
+DJANGO_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -40,6 +54,22 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 ]
+
+# Aplicativos de terceiros
+THIRD_APPS = [
+    #...
+]
+
+# Nossos Apps
+PROJECT_APPS = [
+    #'apps.base',
+    
+    #'apps.myapp',
+]
+INSTALLED_APPS = DJANGO_APPS + THIRD_APPS + PROJECT_APPS
+
+
+ALLOWED_HOSTS = []
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -75,14 +105,18 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-# Database
+# Banco de Dados.
 DATABASES = {
-'default': {
-'ENGINE': 'django.db.backends.sqlite3',
-'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, os.getenv('NAME_DB')),
+        #'USER':os.getenv('USER_DB')
+        #'PASSWORD': os.getenv('PASSWORD_DB')
+        #'HOST':os.getenv('HOST_DB')
+        #'PORT':os.getenv('PORT_DB')
+    }
 }
-}
-
+ 
 
 
 # Password validation
